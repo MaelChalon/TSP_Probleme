b72d58b9e3944cefbb4d106679a97a5c75a916a8
Ugueau <hugomillot2003@gmail.com>
2026-03-23 09:40:36 +0100

    add a city class to keep cities ids

--- TSP.py
+++ TSP.py
@@ -1,20 +1,32 @@
 import random
-def create_random_population(cities : list, size : int):
+
+
+class City:
+    id : int
+    coord : tuple[float, float]
+    
+    def __init__(self, id, coord :tuple[float, float]):
+        self.id = id
+        self.coord = coord
+    def __repr__(self):
+        return f"City(id={self.id}, coord={self.coord})"
+
+def create_random_population(cities : list[City], size : int):
     pop = []
     for _ in range(size):
         pop.append(create_sample_random(cities))
     print(f"New population created : {pop}")
     return pop
 
-def create_sample_random(cities : list):
+def create_sample_random(cities : list[City]):
     sample = cities
     shuffled = random.sample(sample, len(sample))
     print(f"sample created : {shuffled}")
     return shuffled
 
-*points = [
-    (48.8566, 2.3522),
-    (50.1109, 8.6821),
-    (52.5200, 13.4050)
+points = [
+    City(1, (48.8566, 2.3522)),
+    City(2,(50.1109, 8.6821)),
+    City(3, (52.5200, 13.4050))
 ]
 create_random_population(points, 5)
